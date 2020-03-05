468a1b686541d7d9c423d73c1fd445fa7f9e8088
Radoslaw <beziek120@o2.pl>
2020-03-05 12:34:03 +0100

    test

---
+++ start.py
@@ -0,0 +1,19 @@
+#zad 3
+a=8
+b=4
+dodawanie = a + b
+print(dodawanie)
+odejmowanie=a-b
+print(odejmowanie)
+mnozenie=a*b
+print(mnozenie)
+dzielenie=a//b
+print(dzielenie)
+potega=a**b
+print(potega)
+procenty=a%b
+print(procenty)
+
+## zad 4
+
+# testing hehe
